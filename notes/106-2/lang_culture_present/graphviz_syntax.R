75a6d8225ff99afaabd4897c91e8b008dd22575c
liao <liaomovie2@gmail.com>
2018-06-23 11:52:38 +0800

    up

---
+++ notes/106-2/lang_culture_present/graphviz_syntax.R
@@ -0,0 +1,29 @@
+library(DiagrammeR)
+
+var <- 1:3
+
+grViz("
+digraph { //non-directed graph
+      graph [layout = neato]  //layout = dot|neato|twopi|circo|fdp
+      rankdir=BT    // TB|RL|LR, LR: Left to Right orientation
+
+      node [shape = circle,
+      style = filled,
+      color = orange]
+      
+      node [fillcolor = red]
+      a [label = '受']
+      
+      node [fillcolor = orange1]  //orange1-4
+      b [label = '@@1-1']
+      c [label = '@@1-2']
+      d [label = '@@1-3']
+      
+      edge [color = grey, arrowhead = none, arrowtail = none]
+      {b c d} -> a
+      }
+      
+      # Comments for Ref: vector of length=3
+      [1]: paste('Number: ', var, sep='')   
+
+      ")
